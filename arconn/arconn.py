6b7a1d4fb9e2acdd1c3ede02d550095a5283ba48
rehmanjameel <abdulrehmancs17@gmail.com>
2022-04-02 18:09:14 +0500

    gpio library not required

--- arconn/arconn.py
+++ arconn/arconn.py
@@ -1,14 +1,19 @@
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
 import sched
 from datetime import date, datetime, timedelta
 from suntime import Sun, SunTimeException
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(20, GPIO.OUT)
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(20, GPIO.OUT)
 
 sun_set_seconds = None
 sun_rise_seconds = None
+
+BASE_GPIO = '/sys/class/gpio'
+PATH_GPIO = os.path.join(BASE_GPIO, 'gpio{}')
+GPIO_OUT_ON = 0
+GPIO_OUT_OFF = 1
 
 def delay_timings():
     delay_timings.s = sched.scheduler(time.time, time.sleep)
@@ -22,8 +27,8 @@ def get_sun_timings():
     current_latitude1 = 30.1979793
     current_longitude1 = 71.4724978
 
-    print("Latitude = ", current_latitude1)
-    print("Longitude = ", current_longitude1)
+    # print("Latitude = ", current_latitude1)
+    # print("Longitude = ", current_longitude1)
 
     get_sun_time = Sun(current_latitude1, current_longitude1)
     # current date time
@@ -50,24 +55,74 @@ def light_on_off(time_):
 
 
 def light_on(time_):
-    if GPIO.HIGH:
+    # if GPIO.HIGH:
         #print("On")
-        GPIO.output(20, GPIO.LOW)
+        # GPIO.output(20, GPIO.LOW)
+    ARConn.set_out_high(20)
+
     delay_timings.s.enter(sun_set_seconds, 1, light_on, (time_,))
 
 
 def light_off(time_):
     #if GPIO.LOW:
     print("Off")
-    GPIO.output(20, GPIO.HIGH)
+    # GPIO.output(20, GPIO.HIGH)
+    ARConn.set_out_low(20)
+
     delay_timings.s.enter(sun_rise_seconds, 1, light_off, (time_,))
 
 
 class ARConn:
     delay_timings()
+
+    # deskconn piconn functions
+    def _set(self, direction, pin_number, value):
+        path = PATH_GPIO.format(pin_number)
+        if os.path.exists(path):
+            with open(os.path.join(path, 'direction'), 'w') as file:
+                file.write(direction)
+            with open(os.path.join(path, 'value'), 'w') as file:
+                file.write(str(value))
+            return True
+        else:
+            print("Path {} does not exist".format(path))
+            return False
+
+    def set_out_high(self, pin_number):
+        done = self._set('out', pin_number, GPIO_OUT_ON)
+        # if done:
+        #     self.publish('org.deskconn.piconn.gpio.on_out_high', pin_number)
+
+    def set_out_low(self, pin_number):
+        done = self._set('out', pin_number, GPIO_OUT_OFF)
+        # if done:
+            # self.publish('org.deskconn.piconn.gpio.on_out_low', pin_number_number)
+
+    def get_state(self, pin_number):
+        path = PATH_GPIO.format(pin_number)
+        with open(os.path.join(path, 'direction')) as file:
+            direction = file.read().strip()
+        with open(os.path.join(path, 'value')) as file:
+            value = file.read().strip()
+
+        return {
+            'pin_number': pin_number,
+            'direction': direction,
+            'value': value,
+            'value_verbose': 'on' if value == '0' else 'off'
+        }
+
+    def get_states(self):
+        def is_gpio(item):
+            return item.startswith('gpio') and 'chip' not in item
+
+        return [self.get_state(int(pin.replace('gpio', ''))) for pin in filter(is_gpio, os.listdir(BASE_GPIO))]
+
+
     try:
         time_ = delay_timings.s
 
+        get_states()
         # Calling func for getting the sun set and rise time in seconds to set the scheduler
         get_sun_timings()
 
@@ -83,4 +138,5 @@ class ARConn:
         # Running scheduler
         delay_timings.s.run()
     finally:
-        GPIO.cleanup()
+        print("Closed")
+        # GPIO.cleanup()
