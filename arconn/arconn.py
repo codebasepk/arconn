4a4fd87fa7ee326d7dc0ce00ea7b7b5a36bb7809
rehmanjameel <abdulrehmancs17@gmail.com>
2022-04-06 16:24:27 +0500

    schedule library changed

--- arconn/arconn.py
+++ arconn/arconn.py
@@ -1,19 +1,25 @@
 import time
-import sched
-from datetime import datetime, timedelta
+from datetime import datetime, time
 
 from suntime import Sun
+import timesched
 
-from arconn.gpio_control import GPIO_OUT_OFF, GPIO_OUT_ON, set_out_high, set_out_low
+from arconn.gpio_control import set_out_high, set_out_low
 
-sun_set_seconds = None
-sun_rise_seconds = None
+sun_set_hours = None
+sun_set_minutes = None
+sun_rise_hours = None
+sun_rise_minutes = None
 
-light_scheduler = sched.scheduler(time.time, time.sleep)
-now = datetime.now()
+light_scheduler = timesched.Scheduler()
 
-run_at = now + timedelta(hours=12)
-delay = (run_at - now).total_seconds()
+
+def light_on():
+    set_out_high(20)
+
+
+def light_off():
+    set_out_low(20)
 
 
 def get_sun_timings():
@@ -26,46 +32,39 @@ def get_sun_timings():
 
     sun_rise_time = get_sun_time.get_local_sunrise_time(today_date)
     sun_set_time = get_sun_time.get_local_sunset_time(today_date)
+    print(sun_set_time)
 
-    global sun_set_seconds
-    global sun_rise_seconds
+    global sun_set_hours
+    global sun_set_minutes
+    global sun_rise_hours
+    global sun_rise_minutes
 
-    sun_set_seconds = sun_set_time.timestamp()
-    sun_rise_seconds = sun_rise_time.timestamp()
+    sun_set_hours = sun_set_time.strftime("%H")
+    sun_set_minutes = sun_set_time.strftime("%M")
+    sun_rise_hours = sun_rise_time.strftime("%I")
+    sun_rise_minutes = sun_rise_time.strftime("%M")
 
 
-def light_on_off(time_):
+def light_on_off():
     get_sun_timings()
-    light_scheduler.enter(delay, 1, light_on_off, (time_,))
 
-
-def light_on(time_):
-    set_out_high(20)
-    light_scheduler.enter(sun_set_seconds, 1, light_on, (time_,))
-
-
-def light_off(time_):
-    set_out_low(20)
-    light_scheduler.enter(sun_rise_seconds, 1, light_off, (time_,))
+    # 2nd scheduler to turn light on/off
+    light_scheduler.repeat_on_days('MTWTFSs', time(sun_set_hours, sun_set_minutes), 0, light_on)
+    light_scheduler.repeat_on_days('MTWTFSs', time(sun_rise_hours, sun_rise_minutes), 0, light_off)
+    # run scheduler
+    light_scheduler.run()
 
 
 class ARConn:
     try:
-        time_ = light_scheduler
-
         # getting  sun set and rise time in seconds to set the scheduler
         get_sun_timings()
 
-        def __init__(self, time_):
-            self.time = time_
-
         # Scheduling sun set and rise time
-        light_scheduler.enter(delay, 1, light_on_off, (light_scheduler,))
-        # Scheduling light on
-        light_scheduler.enterabs(sun_set_seconds, 1, light_on, (light_scheduler,))
-        # Scheduling light off
-        light_scheduler.enterabs(sun_rise_seconds, 1, light_off, (light_scheduler,))
+        light_scheduler.repeat_on_days('MTWTFSs', time(16, 5), 0, light_on_off)
+
         # Running scheduler
         light_scheduler.run()
+
     finally:
         print("Closed")
