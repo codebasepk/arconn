036f5cbf07248ef88e94ae420ebf3efd73e02471
rehmanjameel <abdulrehmancs17@gmail.com>
2022-04-30 16:41:02 +0500

    schedular-not-required, asyncio-added

--- arconn/arconn.py
+++ arconn/arconn.py
@@ -1,70 +1,95 @@
-import time
-from datetime import datetime, time
-
+from datetime import datetime
 from suntime import Sun
-import timesched
-
+from autobahn.twisted.wamp import ApplicationSession
 from arconn.gpio_control import set_out_high, set_out_low
-
-sun_set_hours = None
-sun_set_minutes = None
-sun_rise_hours = None
-sun_rise_minutes = None
-
-light_scheduler = timesched.Scheduler()
+import asyncio
 
 
-def light_on():
-    set_out_high(20)
+class ARConn(ApplicationSession):
 
+    def __init__(self, config=None):
+        super().__init__(config)
+        self.current_latitude = 30.1979793
+        self.current_longitude = 71.4724978
+        self.wamp_publish = False
 
-def light_off():
-    set_out_low(20)
+    @staticmethod
+    async def light_on():
+        """function from gpio_control module to turn light on"""
+        print("light on")
+        set_out_high(20)
 
+    @staticmethod
+    async def light_off():
+        """function from gpio_control module to turn light off"""
+        print("light off")
+        set_out_low(20)
 
-def get_sun_timings():
-    current_latitude1 = 30.1979793
-    current_longitude1 = 71.4724978
+    # Autobahn connection
+    async def onJoin(self, details):
+        # 1. subscribe to a topic so we receive events
+        async def on_event(msg):
+            print(details)
+            self.wamp_publish = False
+            print(self.wamp_publish)
+            print("Got event: {}".format(msg))
+            if msg == "on":
+                await self.light_on()
+            elif msg == "off":
+                await self.light_off()
 
-    get_sun_time = Sun(current_latitude1, current_longitude1)
-    # current date time
-    today_date = datetime.today()
+        await self.subscribe(on_event, 'org.codebase')
+        print("subscribed")
 
-    sun_rise_time = get_sun_time.get_local_sunrise_time(today_date)
-    sun_set_time = get_sun_time.get_local_sunset_time(today_date)
-    print(sun_set_time)
+    async def get_sun_time(self):
+        """get_sun_time function gets the sun set and rise time every time using suntime library
+        when it's being called from start function for creating asyncio task"""
+        # suntime library to get the sun timing according to current location
+        get_sun_time = Sun(self.current_latitude, self.current_longitude)
 
-    global sun_set_hours
-    global sun_set_minutes
-    global sun_rise_hours
-    global sun_rise_minutes
+        # current date time
+        today_date = datetime.today()
 
-    sun_set_hours = sun_set_time.strftime("%H")
-    sun_set_minutes = sun_set_time.strftime("%M")
-    sun_rise_hours = sun_rise_time.strftime("%I")
-    sun_rise_minutes = sun_rise_time.strftime("%M")
+        # methods to get the
+        sun_rise_time = get_sun_time.get_local_sunrise_time(today_date)
+        sun_set_time = get_sun_time.get_local_sunset_time(today_date)
 
+        print(sun_rise_time)
+        sun_rise_seconds = sun_rise_time.timestamp()
+        sun_set_seconds = sun_set_time.timestamp()
 
-def light_on_off():
-    get_sun_timings()
+        # calling function light_on_off to pass the sun set and rise time in seconds as arguments
+        await self.light_on_off(sun_set_seconds, sun_rise_seconds)
 
-    # 2nd scheduler to turn light on/off
-    light_scheduler.repeat_on_days('MTWTFSs', time(sun_set_hours, sun_set_minutes), 0, light_on)
-    light_scheduler.repeat_on_days('MTWTFSs', time(sun_rise_hours, sun_rise_minutes), 0, light_off)
-    # run scheduler
-    light_scheduler.run()
+    async def light_on_off(self, set_seconds, rise_seconds):
+        """light_on_off function getting the sun set and rise time in seconds from get_sun_time function
+        to manage the tasks to turn light on and off on a specific time according to current time"""
+        cur_time = datetime.now()
+        cur_time_in_sec = cur_time.timestamp()
 
+        rise_remaining_time = rise_seconds - cur_time_in_sec
+        set_remaining_time = set_seconds - cur_time_in_sec
 
-class ARConn:
-    try:
-        # getting  sun set and rise time in seconds to set the scheduler
-        get_sun_timings()
+        if self.wamp_publish:
+            if set_seconds <= cur_time_in_sec >= rise_seconds:
+                light_on_task = asyncio.create_task(self.scheduling_day_sec(set_remaining_time, self.light_on))
+                await light_on_task
+            elif rise_seconds <= cur_time_in_sec <= set_seconds:
+                light_off_task = asyncio.create_task(self.scheduling_day_sec(rise_remaining_time, self.light_off))
+                await light_off_task
 
-        # Scheduling sun set and rise time
-        light_scheduler.repeat_on_days('MTWTFSs', time(3, 00), 0, light_on_off)
+    @staticmethod
+    async def scheduling_day_sec(timeout, get_sun_time):
+        await asyncio.sleep(timeout)
+        await get_sun_time()
 
-        # Running scheduler
-        light_scheduler.run()
+    async def start(self):
 
-    finally:
-        print("Closed")
+        try:
+            while True:
+                task = asyncio.create_task(self.scheduling_day_sec(10, self.get_sun_time))
+                self.wamp_publish = True
+                await task
+
+        finally:
+            print("Closed")
