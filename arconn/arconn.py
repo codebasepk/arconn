09228e6c7072d5ff5fbfb6fde4c51f08b40bea7c
rehmanjameel <abdulrehmancs17@gmail.com>
2022-05-13 16:59:41 +0500

    Both automatic and manual mode working

--- arconn/arconn.py
+++ arconn/arconn.py
@@ -16,28 +16,32 @@ class Setter:
 
     async def call_with_delay(self, delay_seconds, callback):
         await asyncio.sleep(delay_seconds)
-        callback()
+        await callback()
 
     async def light_on(self):
-        """function from gpio_control module to turn light on"""
+        """function from gpio_control module to turn light on
+        """
         await self.cancel()
 
-        print("light on")
         set_out_high(20)
 
     async def light_off(self):
-        """function from gpio_control module to turn light off"""
+        """function from gpio_control module to turn light off
+        """
         await self.cancel()
 
-        print("light off")
         set_out_low(20)
 
     async def set_on_at(self, delay_seconds):
+        """set_on_at function get called by client device to turns
+        'light-on' at specific time, received from client device
+        """
+
         await self.cancel()
         cur_time = datetime.now()
         cur_time_in_sec = cur_time.timestamp()
         set_delay_seconds = delay_seconds - cur_time_in_sec
-        print("here")
+
         self.task = asyncio.create_task(self.call_with_delay(set_delay_seconds, self.light_on))
         self.task_type = "on"
         await self.task
@@ -46,11 +50,16 @@ class Setter:
         asyncio.run(self.set_on_at(delay_seconds))
 
     async def set_off_at(self, delay_seconds):
+        """set_on_at function get called by client device
+            to schedule the specific time to turns 'light-off',
+            received from client device
+            """
+
         await self.cancel()
         cur_time = datetime.now()
         cur_time_in_sec = cur_time.timestamp()
         set_delay_seconds = delay_seconds - cur_time_in_sec
-        print("in off")
+
         self.task = asyncio.create_task(self.call_with_delay(set_delay_seconds, self.light_off))
         self.task_type = "off"
         await self.task
@@ -59,8 +68,6 @@ class Setter:
         asyncio.run(self.set_off_at(delay_seconds))
 
     def is_set(self):
-        print(self.task, "task")
-        print(type(self.task))
         return self.task is not None and not self.task.done()
 
     def current_state(self):
@@ -79,26 +86,15 @@ class ARConn(ApplicationSession):
         super().__init__(config)
         self.current_latitude = 30.1979793
         self.current_longitude = 71.4724978
-        self.current_task_type = None
+        self.task = None
         self.setter = Setter()
 
-    # Autobahn connection
-    async def onJoin(self, details):
-
-        reg = await self.register(self.start, "org.codebase.sys.automatically_on_off")
-        self.log.info("Registered procedure {procedure}", procedure=reg.procedure)
-        reg = await self.register(self.setter.light_on, "pk.codebase.sys.light_on")
-        self.log.info("Registered procedure {procedure}", procedure=reg.procedure)
-        reg = await self.register(self.setter.light_off, "pk.codebase.sys.light_off")
-        self.log.info("Registered procedure {procedure}", procedure=reg.procedure)
-        reg = await self.register(self.setter.run_set_on, "pk.codebase.sys.set_on_at")
-        self.log.info("Registered procedure {procedure}", procedure=reg.procedure)
-        reg = await self.register(self.setter.run_set_off, "pk.codebase.sys.set_off_at")
-        self.log.info("Registered procedure {procedure}", procedure=reg.procedure)
-
     def get_sun_times(self):
-        """get_sun_time function gets the sun set and rise time every time using suntime library
-        when it's being called from start function for creating asyncio task"""
+        """get_sun_time function gets the sun set and rise time
+        every time using suntime library when it's being called
+        from start function for creating asyncio task
+        """
+
         # suntime library to get the sun timing according to current location
         get_sun_time = Sun(self.current_latitude, self.current_longitude)
 
@@ -109,7 +105,6 @@ class ARConn(ApplicationSession):
         sun_rise_time = get_sun_time.get_local_sunrise_time(today_date)
         sun_set_time = get_sun_time.get_local_sunset_time(today_date)
 
-        print(sun_rise_time)
         sun_rise_seconds = sun_rise_time.timestamp()
         sun_set_seconds = sun_set_time.timestamp()
 
@@ -122,21 +117,24 @@ class ARConn(ApplicationSession):
         await self.light_on_off(sun_set_seconds, sun_rise_seconds)
 
     async def light_on_off(self, set_seconds, rise_seconds):
-        """light_on_off function getting the sun set and rise time in seconds from get_sun_time function
-        to manage the tasks to turn light on and off on a specific time according to current time"""
+        """light_on_off function getting the sun set and rise time in seconds
+        from set_sun_time function to manage the tasks to turn light on and
+        off on a specific time according to current time
+        """
+
         cur_time = datetime.now()
         cur_time_in_sec = cur_time.timestamp()
 
-        if set_seconds <= cur_time_in_sec >= rise_seconds:
-            await self.setter.light_on()
+        if set_seconds <= cur_time_in_sec <= rise_seconds:
+            # this condition will check that if current time greater than sun-set-time and less than sun-rise-time
+            # then it will set the time to turn light off
             rise_remaining_time = rise_seconds - cur_time_in_sec
-
             light_off_task = asyncio.create_task(self.scheduling_day_sec(rise_remaining_time, self.setter.light_off))
             await light_off_task
-        elif rise_seconds <= cur_time_in_sec <= set_seconds:
-            await self.setter.light_off()
+        elif rise_seconds <= cur_time_in_sec >= set_seconds:
+            # this condition will check that if current time less than sun-set-time and greater than sun-rise-time
+            # then it will set the time to turn light on
             set_remaining_time = set_seconds - cur_time_in_sec
-
             light_on_task = asyncio.create_task(self.scheduling_day_sec(set_remaining_time, self.setter.light_on))
             await light_on_task
 
@@ -145,12 +143,41 @@ class ARConn(ApplicationSession):
         await sleep(timeout)
         await set_sun_time()
 
-    async def start(self):
+    async def start(self, set_task):
+        # run self.set_sun_time() function first time when asyncio start
+        # to check the current state according to current time
         await self.set_sun_time()
 
         try:
-            while True:
-                task = asyncio.create_task(self.scheduling_day_sec(43200, self.set_sun_time))
-                await task
+            while set_task:
+                self.task = asyncio.create_task(self.scheduling_day_sec(43200, self.set_sun_time))
+                await self.task
         finally:
-            print("Closed")
+            print("Cancelled")
+            # main task get cancelled ss set_task variable set 'False'
+            # for cancelling the task it will receive 'false' argument from client device
+            self.cancel_main_task()
+
+    def is_set(self):
+        return self.task is not None and not self.task.done()
+
+    def cancel_main_task(self):
+        if self.is_set():
+            self.task.cancel()
+
+    # Autobahn connection
+    async def onJoin(self, details):
+        """onJoin is an autobahn async function registering procedures
+        for calling them from client device to perform the specific tasks
+        """
+
+        reg = await self.register(self.start, "pk.codebase.sys.automatically_on_off")
+        self.log.info("Registered procedure {procedure}", procedure=reg.procedure)
+        reg = await self.register(self.setter.light_on, "pk.codebase.sys.light_on")
+        self.log.info("Registered procedure {procedure}", procedure=reg.procedure)
+        reg = await self.register(self.setter.light_off, "pk.codebase.sys.light_off")
+        self.log.info("Registered procedure {procedure}", procedure=reg.procedure)
+        reg = await self.register(self.setter.run_set_on, "pk.codebase.sys.set_on_at")
+        self.log.info("Registered procedure {procedure}", procedure=reg.procedure)
+        reg = await self.register(self.setter.run_set_off, "pk.codebase.sys.set_off_at")
+        self.log.info("Registered procedure {procedure}", procedure=reg.procedure)
